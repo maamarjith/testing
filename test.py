02e3f7726b0ae47a94dc4e7334daadf025026b3c
maamarjtih <amarjithma2@gmail.com>
2025-05-14 19:29:31 +0530

    Calculator

--- test.py
+++ test.py
@@ -1,9 +1,22 @@
-print("Hi")
-print("Hi")
-print("Hi")
-print("Hi")
-print("Hi")
-print("Hi")
-print("Hi")
-print("Hi")
-print("Hi")
+print ("Hi User! Welcome Back")
+print("Let us create some sample project")
+print("Simple Calculator")
+
+
+
+choice = int(input("1 for addition, 2 for multiplication & 0 for exit "))
+while(True):
+    if choice ==1:
+        num1 = int(input("Enter the value: "))
+        num2 = int(input("Enter the value: "))
+        print(f"Sum of {num1} + {num2} = {num1+num2}")
+    elif choice == 2:
+        num1 = int(input("Enter the value: "))
+        num2 = int(input("Enter the value: "))
+        print(f"Sum of {num1} * {num2} = {num1*num2}")
+    elif choice == 0:
+       print("Bye, see you again")
+       break
+    else:
+        print("Invalid input, Please try again")
+
